c50296b888470d5b960300e90bf8aa571e31ab9f
aman Gupta <97275831+Amanraj24@users.noreply.github.com>
2022-07-22 08:10:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,3 +75,9 @@ for i in range(100):
   time.sleep(0.1)
 
 '...and now we\'re done!'
+import streamlit as st
+
+@st.cache  # 👈 This function will be cached
+def my_slow_function(arg1, arg2):
+    # Do something really slow in here!
+    return the_output
