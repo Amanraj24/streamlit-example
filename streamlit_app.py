885ac479e8891f1c871f89a52dc07d0c99986a42
aman Gupta <97275831+Amanraj24@users.noreply.github.com>
2022-07-22 07:59:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,3 +18,7 @@ map_data = pd.DataFrame(
 st.map(map_data)
 x = st.slider('x')  # 👈 this is a widget
 st.write(x, 'squared is', x * x)
+st.text_input("Your name", key="name")
+
+# You can access the value at any point with:
+st.session_state.name
